79941b8a0b8112139f126a270f3bba7d2509275a
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2026-07-11 19:35:44 +0530

    modules/home/tools: Configure SSH and CLI Home tools

--- modules/home/tools/git.nix
+++ modules/home/tools/git.nix
@@ -1,4 +1,22 @@
 { ... }:
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "Kasim Kaizer";
+      email = "155087257+KasimKaizer@users.noreply.github.com";
+    };
+    signing.format = "openpgp";
+  };
+
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "https";
+      prompt = "enabled";
+      aliases.co = "pr checkout";
+    };
+  };
+
+  programs.lazygit.enable = true;
 }
