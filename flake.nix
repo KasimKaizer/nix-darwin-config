bdbae7beaf608260958c77bf29c80280b11112f6
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2026-07-11 18:58:02 +0530

    secrets: Add sops-nix managed secrets

--- flake.nix
+++ flake.nix
@@ -10,6 +10,9 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
