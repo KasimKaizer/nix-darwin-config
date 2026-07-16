690eda30ce66556b7a658db88507308a1d3ec8ed
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2026-07-16 17:06:44 +0530

    darwin: Capture defaults, Dock, and app hotkeys
    Match live Finder, trackpad, WindowManager, screenshot, and Safari
    prefs; pin Dock apps; enable firewall and PAM reattach; add skhd
    chords for common apps.

---
+++ modules/darwin/defaults/freedom.nix
@@ -0,0 +1,101 @@
+# Freedom (com.80pct.FreedomPlatform) preferences.
+{
+  ActivityMonitorBlockingEnabled = true;
+  APIDomain = "freedom.to";
+  KillBlockedApps = true;
+  NonProxyMode = false;
+  RecentSessionTimesUsed = [
+    25
+    2
+    30
+    60
+    120
+  ];
+  SessionBlockApps = false;
+  SessionEndSound = true;
+  SessionStartSound = true;
+  StrongModeEnabled = false;
+  WebsiteExceptionEnabled = false;
+  WebsiteExceptions = [ ];
+
+  BlockedApps = [
+    "Balatro.app"
+    "BlueWallet.app"
+    "CleanMyMac_5.app"
+    "Discord.app"
+    "FreeTube.app"
+    "Hades II.app"
+    "Infuse.app"
+    "Kindle Comic Converter.app"
+    "Kindle Previewer 3.app"
+    "LibreWolf.app"
+    "Minecraft Bedrock Launcher.app"
+    "Outlanders.app"
+    "Plex.app"
+    "Telegram.app"
+    "WhatsApp.app"
+  ];
+
+  BlockedAppInfo = {
+    "Balatro.app" = {
+      bundleID = "com.Balatro.localthunk";
+      name = "Balatro";
+    };
+    "BlueWallet.app" = {
+      bundleID = "io.bluewallet.bluewallet";
+      name = "BlueWallet";
+    };
+    "CleanMyMac_5.app" = {
+      bundleID = "com.macpaw.CleanMyMac5";
+      name = "CleanMyMac_5";
+    };
+    "Discord.app" = {
+      bundleID = "com.hnc.Discord";
+      name = "Discord";
+    };
+    "FreeTube.app" = {
+      bundleID = "io.freetubeapp.freetube";
+      name = "FreeTube";
+    };
+    "Hades II.app" = {
+      bundleID = "com.supergiantgames.macos.hades2";
+      name = "Hades II";
+    };
+    "Infuse.app" = {
+      bundleID = "com.firecore.infuse";
+      name = "Infuse";
+    };
+    "Kindle Comic Converter.app" = {
+      bundleID = "Kindle Comic Converter";
+      name = "Kindle Comic Converter";
+    };
+    "Kindle Previewer 3.app" = {
+      bundleID = "com.amazon.kpr";
+      name = "Kindle Previewer 3.app";
+    };
+    "LibreWolf.app" = {
+      bundleID = "org.mozilla.librewolf";
+      name = "LibreWolf";
+    };
+    "Minecraft Bedrock Launcher.app" = {
+      bundleID = "io.mrarm.mcpelauncher.ui";
+      name = "Minecraft Bedrock Launcher";
+    };
+    "Outlanders.app" = {
+      bundleID = "com.pomelogames.KingdomsMac";
+      name = "Outlanders";
+    };
+    "Plex.app" = {
+      bundleID = "tv.plex.desktop";
+      name = "Plex";
+    };
+    "Telegram.app" = {
+      bundleID = "ru.keepcoder.Telegram";
+      name = "Telegram";
+    };
+    "WhatsApp.app" = {
+      bundleID = "net.whatsapp.WhatsApp";
+      name = "WhatsApp";
+    };
+  };
+}
